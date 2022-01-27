214188872b14825f9cdd71bc031e04d162226c5e
srgokulnath <srgwilet@gmail.com>
2022-01-27 20:01:09 +0530

    second commit
    added some texts to madlib text flow

--- madlib.py
+++ madlib.py
@@ -6,7 +6,7 @@ adjective = input("tell me an adjective")
 drink = input("a drink in mind")
 sidedish = input("what's the sideddish")
  
-madlib = f"I look {word1} when I want! Never yarn on a moving {vehicle} u may not know what are those passed in a stream of air where only u know the world is allways {word2}. \n This may seem doubtful but when {vehicle2} crashes the person inside never wants to {adjective}. Dont hope its a poem but a little bit of {drink} and {sidedish} flow go on part"
+madlib = f"I look {word1} when I want! Never yarn on a moving {vehicle} u may not know what are those passed in a stream of air where only u know the world is allways {word2}. \n This may seem doubtful but when {vehicle2} crashes the person inside never wants to {adjective}. Dont hope its a poem but a little bit of {drink} and {sidedish} flow go on part. I added some more check the roop reflection"
 
 print(madlib)
 
