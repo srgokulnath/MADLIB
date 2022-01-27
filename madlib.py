5be965be27a8e7c40f57e8e84fc74b212626675a
srgokulnath <srgwilet@gmail.com>
2022-01-27 19:56:11 +0530

    First commit

---
+++ madlib.py
@@ -0,0 +1,16 @@
+word1 = input("Enter a location")
+vehicle = input("Vehicle in ur mind")
+word2 = input("when ur upset")
+vehicle2 = input("another vehicle")
+adjective = input("tell me an adjective")
+drink = input("a drink in mind")
+sidedish = input("what's the sideddish")
+ 
+madlib = f"I look {word1} when I want! Never yarn on a moving {vehicle} u may not know what are those passed in a stream of air where only u know the world is allways {word2}. \n This may seem doubtful but when {vehicle2} crashes the person inside never wants to {adjective}. Dont hope its a poem but a little bit of {drink} and {sidedish} flow go on part"
+
+print(madlib)
+
+
+ 
+        
+           
